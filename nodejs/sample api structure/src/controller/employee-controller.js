e9c9056504036ce17d397662e820954071e44231
taqi4 <hkjigar3@gmail.com>
2022-02-15 17:26:46 +0530

    req headers error fixed

--- nodejs/sample api structure/src/controller/employee-controller.js
+++ nodejs/sample api structure/src/controller/employee-controller.js
@@ -13,10 +13,15 @@ const getEmployees = async (req, res) => {
         //     return recordset;
             
         //});
-        console.log( req.Header.Get("X-Real-Ip")        );
+        
+        //console.log( req.Header.Get("X-Real-Ip")  );
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(ip); 
+       // console.log(req);
+  console.log(req.headers); 
 
         const users = "taqi";
-        res.send(users).status(200);
+        res.send(users+ip).status(200);
     }catch( error ){
         res.status(404).json({ message: error.message })
     }
@@ -33,9 +38,10 @@ const addEmployee = async (req, res) => {
         //     return recordset;
             
         //});
-        console.log(req.RemoteAddr);
+         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(req.headers); 
         const users = "taqi post";
-        res.send(users).status(200);
+        res.send(users+ip).status(200);
     }catch( error ){
         response.status(404).json({ message: error.message })
     }
